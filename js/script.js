b98202a36434233a45fefb47d24dc57bdd99e98c
Funx <Funx@Funx-PC>
2014-02-20 16:26:21 +0100

    Scripting du menu principal

--- js/script.js
+++ js/script.js
@@ -3,19 +3,39 @@ $(function() {
 
 
 //VARS
-	var sousMenu = $(".submenu > div").slideUp();
-	var categorie = $("#menu a");
+	var $sousMenu = $(".submenu > div").hide();
+	var $categorie = $("#menu a");
+	var $overlay = $("#overlay");
+
 	//EVENTS
-	categorie.on("click", function() {
-		var href = $(this).attr('href');
-		sousMenu.not(href).slideUp("normal",function(){
-					sousMenu.filter(href).slideToggle();
-		});
-		return false;
-	});
+
+	//prevent default sur les ancres vides
 	$('a').on("click", function() {
 		if ($(this).attr('href') === "#") {
 			return false;
 		}
 	});
+
+	//déploiement du menu
+	$categorie.on("click", function() {
+		var href = $(this).attr('href');
+		$sousMenu.not(href).slideUp("normal", function() {
+			if ($sousMenu.filter(href).is(":visible")) {
+				$sousMenu.filter(href).slideUp();
+				$overlay.removeClass('visible');
+			} else {
+				$sousMenu.filter(href).slideDown();
+				$overlay.addClass('visible');
+			}
+		});
+		return false;
+	});
+
+//au clic sur l'overlay
+	$overlay.on("click", function() {
+		$sousMenu.slideUp();
+		$overlay.removeClass('visible');
+	});
+
+
 });
