b7d0e073dbb702f33f746ec5d6f5556be921263e
Funx <Funx@Funx-PC>
2014-02-21 10:35:19 +0100

    Début de dropdown en format xs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(function() {
 	var $categorie = $("#menu a");
 	var $overlay = $("#overlay");
 	var $sideBarController = $('#showSideBar.button');
-
+	var $layout = $('#layout');
 	//EVENTS
 
 	//prevent default sur les ancres vides
@@ -20,6 +20,7 @@ $(function() {
 	//déploiement du menu
 	$categorie.on("click", function() {
 		var href = $(this).attr('href');
+		$layout.removeClass('visible');
 		$sousMenu.not(href).slideUp("normal", function() {
 			if ($sousMenu.filter(href).is(":visible")) {
 				$sousMenu.filter(href).slideUp();
@@ -33,13 +34,17 @@ $(function() {
 	});
 	
 	$sideBarController.on("click",function(){
+			$sousMenu.slideUp();
 		$("#layout").toggleClass('visible');
+				return false;
+
 	});
 
 //au clic sur l'overlay
 	$overlay.on("click", function() {
 		$sousMenu.slideUp();
 		$overlay.removeClass('visible');
+		$layout.removeClass('visible');
 	});
 
 
